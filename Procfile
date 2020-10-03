13c154909956fe22c48e7c1b82418a9939a0331f
Guenole de Cadoudal <guenoledc@yahoo.fr>
2020-10-03 21:01:16 +0200

    add procfile

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: python -m swagger_server
--- swagger_server/__main__.py
+++ swagger_server/__main__.py
@@ -10,8 +10,8 @@ def main():
     app = connexion.App(__name__, specification_dir='./swagger/')
     app.app.json_encoder = encoder.JSONEncoder
     app.add_api('swagger.yaml', arguments={'title': 'French Dictionary'}, pythonic_params=True)
-    port = int(os.environ["PORT"]) if "PORT" in os.environ else 8080
-    app.run(port=port)
+    port = int(os.environ.get("PORT", 8080))
+    app.run(port=port, host="0.0.0.0")
 
 
 if __name__ == '__main__':
