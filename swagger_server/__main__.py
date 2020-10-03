9a0a2a20fa2a15e36c4b89c0355ceb3d216e4a93
Guenole de Cadoudal <guenoledc@yahoo.fr>
2020-10-03 20:38:39 +0200

    try deploy

--- swagger_server/__main__.py
+++ swagger_server/__main__.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import connexion
+import os
 
 from swagger_server import encoder
 
@@ -9,7 +10,8 @@ def main():
     app = connexion.App(__name__, specification_dir='./swagger/')
     app.app.json_encoder = encoder.JSONEncoder
     app.add_api('swagger.yaml', arguments={'title': 'French Dictionary'}, pythonic_params=True)
-    app.run(port=8080)
+    port = int(os.environ["PORT"]) if "PORT" in os.environ else 8080
+    app.run(port=port)
 
 
 if __name__ == '__main__':
