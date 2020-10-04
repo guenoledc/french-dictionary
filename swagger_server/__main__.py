85a1f2aba16dd5079ec471c260969c3f19151ee3
Guenole de Cadoudal <guenoledc@yahoo.fr>
2020-10-04 10:32:31 +0200

    trace calls

--- swagger_server/__main__.py
+++ swagger_server/__main__.py
@@ -14,8 +14,15 @@ def root(app, api):
         return flask.redirect(api.base_path+app.options.openapi_console_ui_path)
     return func
 
+def beforeRequest():
+    with flask.request as r:
+        print(r.method, r.url, r.environ["REMOTE_ADDR"], dict(r.headers))
+
 def main():
     app = connexion.App(__name__, specification_dir='./swagger/')
+    
+    app.app.before_request(beforeRequest)
+
     app.app.json_encoder = encoder.JSONEncoder
     api = app.add_api('swagger.yaml', arguments={'title': 'French Dictionary'}, pythonic_params=True)
     app.add_url_rule("/", None, root(app, api))
