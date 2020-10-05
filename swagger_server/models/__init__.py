c787ab5e75b2d46ef396d7e8bc9183cd0285930e
Guenole de Cadoudal <guenoledc@yahoo.fr>
2020-10-05 23:42:16 +0200

    first simple version

--- swagger_server/models/__init__.py
+++ swagger_server/models/__init__.py
@@ -5,3 +5,4 @@ from __future__ import absolute_import
 # import models into model package
 from swagger_server.models.error import Error
 from swagger_server.models.word import Word
+from swagger_server.models.words import Words
